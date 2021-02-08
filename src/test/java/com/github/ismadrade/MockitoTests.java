dc3b1bfe9e62fdc187929c22afc30b8cee675f12
Ismael Andrade <maelandrade@yahoo.com.br>
2021-02-08 13:35:12 -0300

    Mais exemplos com mockito

--- src/test/java/com/github/ismadrade/MockitoTests.java
+++ src/test/java/com/github/ismadrade/MockitoTests.java
@@ -3,6 +3,7 @@ package com.github.ismadrade;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -19,8 +20,24 @@ public class MockitoTests {
     @Test
     public void primeiroTesteMockito(){
         Mockito.when( lista.size() ).thenReturn(20);
-        int size = lista.size();
+        int size = 0;
+        if(1 == 1){
+            size = lista.size();
+        }
         Assertions.assertThat(size).isEqualTo(20);
 
+        Mockito.verify(lista).size(); //verifica se o metodo foi chamado
+        Mockito.verify(lista, Mockito.times(1)).size(); //verifica se o metodo foi chamado uma vez
+        //Mockito.verify(lista, Mockito.never()).size(); verifica se o metodo nunca foi chamado
+
+    }
+
+    @Test
+    public void segundoTesteMockito(){
+        lista.size(); //ordem1
+        lista.add(""); //ordem2
+        InOrder inOrder = Mockito.inOrder(lista); //Verifica se os metodos foram executados na ordem do inOrder.
+        inOrder.verify(lista).size(); //ordem1
+        inOrder.verify(lista).add(""); //ordem2
     }
 }
